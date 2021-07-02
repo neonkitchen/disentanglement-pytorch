19f07c181f6a54927e0cda5baae1152b7a0c70b3
Neelan Pather <neelan.pather@gmail.com>
2021-07-02 17:36:31 +0200

    update script

--- run/3dshapes_test.sh
+++ run/3dshapes_test.sh
@@ -2,7 +2,7 @@
 nohup python3 main.py \
 --name=3dshapes_base \
 --alg=BetaVAE \
---dset_dir=/mnt/data/home/neelan/disentanglement-pytorch/data/3dshapes \
+--dset_dir=/mnt/data/home/neelan/data/3dshapes \
 --dset_name=shapes3d \
 --traverse_z=true \
 --encoder=SimpleGaussianConv64 \
@@ -11,6 +11,8 @@ nohup python3 main.py \
 --use_wandb=false \
 --w_kld=1.0 \
 --max_iter=100 \
+--print_iter=10 \
+--recon_iter=50 \
 --batch_size=64 \
 --num_workers=16 \
 >nohup/3dshapes_base.out \
